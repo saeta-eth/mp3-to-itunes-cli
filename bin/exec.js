4ba165a24d1597dfd63f58def512ca62b54377a9
slorenzo <SebastianLorenzo@gmail.com>
2017-05-11 17:58:08 -0300

    Added init script

---
+++ bin/exec.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+var shell = require("shelljs");
+var ConvertItunesCommand = require('../');
+
+var mp3Files = shell.find(shell.pwd()).filter(function(file) {
+  return file.match(/\.mp3$/);
+});
+
+for (var i = 0, len = mp3Files.length; i < len; i++) {
+  mp3Files[i] = {
+    path: mp3Files[i],
+    file: mp3Files[i].split('/').pop()
+  };
+}
+
+if (mp3Files.length) {
+  var ConvertItunes = new ConvertItunesCommand(process.argv[2].split('=').pop(), mp3Files);
+  ConvertItunes.init( function(err, response) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('status: ', response.status);
+      console.log('message: ', response.message);
+      shell.exit(1);
+    }
+  });
+} else {
+  console.error('There are not mp3 files in this directory');
+  shell.exit(1);
+}
+
